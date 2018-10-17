e0d6e415ae2519ca0613437e0589c4a24ebbe0ae
Kyle Romero <kgromero@gmail.com>
2018-10-17 12:19:49 -0500

    Add Particles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Grid, Row, Col, Panel, Table, Image, Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap'
 import NavItem from './CustomReactBootstrap/NavItem'
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
@@ -65,7 +66,7 @@ class App extends Component {
         <Grid>
           <Row>
             <Col xsHidden smHidden md={1} lg={1} />
-            <Col xs={12} sm={12} md={10} lg={10}>
+            <Col xs={12} sm={12} md={10} lg={10} style={{zIndex: 2}}>
               <Panel>
                 <h1>Kyle Romero</h1>
                 <p>Software Development Manager with 10 years experience in IT. Currently living in Houston, TX.</p>
@@ -608,6 +609,121 @@ class App extends Component {
             <Col xsHidden smHidden md={1} lg={1} />
           </Row>
         </Grid>
+        <Particles
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: 0
+            }}
+            params={{
+              "particles": {
+                "number": {
+                  "value": 50,
+                  "density": {
+                    "enable": true,
+                    "value_area": 800
+                  }
+                },
+                "color": {
+                  "value": "#ffffff"
+                },
+                "shape": {
+                  "type": "circle",
+                  "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                  },
+                  "polygon": {
+                    "nb_sides": 5
+                  },
+                },
+                "opacity": {
+                  "value": 0.4,
+                  "random": false,
+                  "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "size": {
+                  "value": 3,
+                  "random": true,
+                  "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                  }
+                },
+                "line_linked": {
+                  "enable": true,
+                  "distance": 150,
+                  "color": "#ffffff",
+                  "opacity": 0.2,
+                  "width": 1
+                },
+                "move": {
+                  "enable": true,
+                  "speed": .5,
+                  "direction": "none",
+                  "random": false,
+                  "straight": false,
+                  "out_mode": "bounce",
+                  "bounce": false,
+                  "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                  }
+                }
+              },
+              "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                  "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                  },
+                  "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                  },
+                  "resize": true
+                },
+                "modes": {
+                  "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                      "opacity": 1
+                    }
+                  },
+                  "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                  },
+                  "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                  },
+                  "push": {
+                    "particles_nb": 4
+                  },
+                  "remove": {
+                    "particles_nb": 2
+                  }
+                }
+              },
+              "retina_detect": true
+            }}
+          />
       </div>
     );
   }
